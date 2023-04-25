be53a17c7269abdbcd66c0f69f44d9e7ba680a38
Nip1706 <payagalage.nipuna@gmail.com>
2023-04-25 18:50:13 +0200

    final

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -8,26 +8,3 @@ orderId.innerHTML = id;
 //On vide le local storage après que la commande soit passée
 localStorage.clear();
 
-/*
-const orderId = getOrderId()
-displayOrderId(orderId)
-deleteAllStorage()
-
-
-function getOrderId() {
-    const queryString = window.location.search
-    const urlParams = new URLSearchParams(queryString)
-    return urlParams.get("orderId")
-}
-
-
-function displayOrderId(orderId) {
-    const elementOrderId = document.getElementById("orderId")
-    elementOrderId.textContent = orderId
-}
-
-function deleteAllStorage() {
-    const storage = window.localStorage
-    storage.clear()
-}
-*/
